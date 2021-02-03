ae276201cc5fc53b03e642e23afaa07fefc64919
Octavian Dima <hello@wopss.net>
2021-02-03 03:56:52 +0200

    Use system DLL to load the extender
    The used system DLL is "version.dll", thus removing the complexity of handling the launchers and also track user's played hours in that specific launcher.
    
    For more information see https://docs.microsoft.com/en-us/windows/win32/dlls/dynamic-link-library-search-order#standard-search-order-for-desktop-applications

--- src/red4ext.loader/Main.cpp
+++ src/red4ext.loader/Main.cpp
@@ -1,35 +1,74 @@
 #include "stdafx.hpp"
-#include "App.hpp"
+#include "VersionDll.hpp"
 
-int wmain(int aArgc, wchar_t* aArgv[], wchar_t* aEnvp[])
+BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
 {
-    RED4ext::App app;
-
-    auto result = app.Init();
-    if (result)
+    switch (aReason)
     {
-        return result.value();
-    }
-
-    // Get all arguments, except the exe path.
-    std::wstringstream cmdLineBuilder;
-    for (int32_t i = 1; i < aArgc; i++)
+    case DLL_PROCESS_ATTACH:
     {
-        cmdLineBuilder << aArgv[i];
-        if (i < aArgc - 1)
+        DisableThreadLibraryCalls(aModule);
+
+        if (!LoadOriginalDll())
         {
-            cmdLineBuilder << L" ";
+            return FALSE;
         }
+
+        constexpr auto EXE = L"Cyberpunk2077.exe";
+        constexpr auto PATH_LENGTH = MAX_PATH + 1;
+        constexpr auto MOD_DIR = L"red4ext";
+        constexpr auto MOD_DLL = L"RED4ext.dll";
+
+        // Try to get the executable path until we can fit the length of the path.
+        std::wstring filename;
+        do
+        {
+            filename.resize(filename.size() + PATH_LENGTH, '\0');
+
+            auto length = GetModuleFileName(nullptr, filename.data(), static_cast<uint32_t>(filename.size()));
+            if (length > 0)
+            {
+                // Resize it to the real, std::filesystem::path" will use the string's length instead of recounting it.
+                filename.resize(length);
+            }
+        } while (GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+
+        // Only load "RED4ext" in game's process.
+        std::filesystem::path exePath(filename);
+        if (exePath.filename() != EXE)
+        {
+            return TRUE;
+        }
+
+        auto rootPath = exePath
+                            .parent_path()  // Resolve to "x64" directory.
+                            .parent_path()  // Resolve to "bin" directory.
+                            .parent_path(); // Resolve to game root directory.
+
+        auto modPath = rootPath / MOD_DIR;
+        if (!std::filesystem::exists(modPath))
+        {
+            auto message = fmt::format(L"The directory '{}' does not exist, RED4ext will not be loaded.\r\n\r\n"
+                                       L"If this is intended please remove 'version.dll' from '{}' directory.",
+                                       modPath.c_str(), exePath.remove_filename().c_str());
+
+            MessageBox(nullptr, message.c_str(), L"RED4ext", MB_ICONWARNING | MB_OK);
+        }
+
+        auto dllPath = modPath / MOD_DLL;
+        if (!LoadLibrary(dllPath.c_str()))
+        {
+            auto message = fmt::format(L"RED4ext could not be loaded, error {}.", GetLastError());
+            MessageBox(nullptr, message.c_str(), L"RED4ext", MB_ICONERROR | MB_OK);
+        }
+
+        break;
     }
-
-    auto cmdLine = cmdLineBuilder.str();
-
-    result = app.Run(cmdLine.data());
-    if (result)
+    case DLL_PROCESS_DETACH:
     {
-        return result.value();
+        break;
+    }
     }
 
-    result = app.Shutdown();
-    return result.value();
+    return TRUE;
 }
--- src/red4ext.loader/stdafx.hpp
+++ src/red4ext.loader/stdafx.hpp
@@ -1,17 +1,9 @@
 #pragma once
 
-#include <cstdint>
 #include <filesystem>
-#include <memory>
-#include <sstream>
 #include <string>
-#include <string_view>
-#include <system_error>
-#include <type_traits>
 
 #include <Windows.h>
 #include <ShlObj_core.h>
 
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/spdlog.h>
+#include <spdlog/fmt/fmt.h>
