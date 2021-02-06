d803ff7e2ce922592ca4b21db02070e921eb0465
Octavian Dima <hello@wopss.net>
2021-02-06 21:23:35 +0200

    Use a mutex to prevent DLL to be loaded twice
    Previously the exe's name would have been checked, this could lead to problems if the user has the exe renamed to something else. A mutex should prevent the problem with naming, Win32 docs recommend to create a locked file in the user's profile directory, but for now this should be enough.

--- src/red4ext.loader/Main.cpp
+++ src/red4ext.loader/Main.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.hpp"
 #include "VersionDll.hpp"
 
+HANDLE s_mutex = nullptr;
+
 BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
 {
     switch (aReason)
@@ -13,17 +15,41 @@ BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
         {
             return FALSE;
         }
+   
+        constexpr auto mutexName = L"RED4ext";
+        s_mutex = CreateMutex(nullptr, true, mutexName);
 
-        constexpr auto EXE = L"Cyberpunk2077.exe";
-        constexpr auto PATH_LENGTH = MAX_PATH + 1;
-        constexpr auto MOD_DIR = L"red4ext";
-        constexpr auto MOD_DLL = L"RED4ext.dll";
+        // Only load "RED4ext" in game's process. The game process is started first and then the crash handler is
+        // created.
+        auto err = GetLastError();
+        if (err == ERROR_ALREADY_EXISTS)
+        {
+            MessageBox(nullptr, L"already exists", L"", MB_OK);
+            return TRUE;
+        }
+        else if (err != ERROR_SUCCESS)
+        {
+            wchar_t* buffer = nullptr;
+            FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                          nullptr, err, LANG_USER_DEFAULT, reinterpret_cast<LPWSTR>(&buffer), 0, nullptr);
+
+            auto message = fmt::format(L"{}\n\nRED4ext could not be loaded, error code 0x{:X}.", buffer, err);
+            MessageBox(nullptr, message.c_str(), L"RED4ext", MB_ICONERROR | MB_OK);
+
+            LocalFree(buffer);
+            buffer = nullptr;
+            return FALSE;
+        }
+
+        constexpr auto pathLength = MAX_PATH + 1;
+        constexpr auto modDir = L"red4ext";
+        constexpr auto modDll = L"RED4ext.dll";
 
         // Try to get the executable path until we can fit the length of the path.
         std::wstring filename;
         do
         {
-            filename.resize(filename.size() + PATH_LENGTH, '\0');
+            filename.resize(filename.size() + pathLength, '\0');
 
             auto length = GetModuleFileName(nullptr, filename.data(), static_cast<uint32_t>(filename.size()));
             if (length > 0)
@@ -33,19 +59,13 @@ BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
             }
         } while (GetLastError() == ERROR_INSUFFICIENT_BUFFER);
 
-        // Only load "RED4ext" in game's process.
         std::filesystem::path exePath(filename);
-        if (exePath.filename() != EXE)
-        {
-            return TRUE;
-        }
-
         auto rootPath = exePath
                             .parent_path()  // Resolve to "x64" directory.
                             .parent_path()  // Resolve to "bin" directory.
                             .parent_path(); // Resolve to game root directory.
 
-        auto modPath = rootPath / MOD_DIR;
+        auto modPath = rootPath / modDir;
         if (!std::filesystem::exists(modPath))
         {
             auto message = fmt::format(L"The directory '{}' does not exist, RED4ext will not be loaded.\r\n\r\n"
@@ -55,7 +75,7 @@ BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
             MessageBox(nullptr, message.c_str(), L"RED4ext", MB_ICONWARNING | MB_OK);
         }
 
-        auto dllPath = modPath / MOD_DLL;
+        auto dllPath = modPath / modDll;
         if (!LoadLibrary(dllPath.c_str()))
         {
             wchar_t* buffer = nullptr;
@@ -75,6 +95,12 @@ BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
     }
     case DLL_PROCESS_DETACH:
     {
+        if (s_mutex)
+        {
+            ReleaseMutex(s_mutex);
+            CloseHandle(s_mutex);
+        }
+
         break;
     }
     }
