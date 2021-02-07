d38bf17d9a538440cd1b74b2f82a6f9517ed0116
Octavian Dima <hello@wopss.net>
2021-02-07 18:23:23 +0200

    Use decoded types for hooked state functions

--- src/red4ext.dll/Hooks/CInitializationState.cpp
+++ src/red4ext.dll/Hooks/CInitializationState.cpp
@@ -3,15 +3,18 @@
 #include "App.hpp"
 #include "REDhook.hpp"
 
+#include <RED4ext/GameApplication.hpp>
+#include <RED4ext/GameStates.hpp>
+
 namespace
 {
-bool CInitializationState_Init(uintptr_t aThis, uintptr_t aApp);
+bool CInitializationState_Init(RED4ext::CInitializationState* aThis, RED4ext::CGameApplication* aApp);
 REDhook<decltype(&CInitializationState_Init)> CInitializationState_Init_h({0x40, 0x55, 0x48, 0x8D, 0x6C, 0x24, 0xA9,
                                                                            0x48, 0x81, 0xEC, 0x90, 0x00, 0x00, 0x00,
                                                                            0xE8, 0xCC, 0xCC, 0xCC, 0xCC},
                                                                           &CInitializationState_Init, 1);
 
-bool CInitializationState_Init(uintptr_t aThis, uintptr_t aApp)
+bool CInitializationState_Init(RED4ext::CInitializationState* aThis, RED4ext::CGameApplication* aApp)
 {
     auto result = CInitializationState_Init_h(aThis, aApp);
 
--- src/red4ext.dll/Hooks/CShutdownState.cpp
+++ src/red4ext.dll/Hooks/CShutdownState.cpp
@@ -3,14 +3,17 @@
 #include "App.hpp"
 #include "REDhook.hpp"
 
+#include <RED4ext/GameApplication.hpp>
+#include <RED4ext/GameStates.hpp>
+
 namespace
 {
-bool CShutdownState_Run(uintptr_t aThis, uintptr_t aApp);
+bool CShutdownState_Run(RED4ext::CShutdownState* aThis, RED4ext::CGameApplication* aApp);
 REDhook<decltype(&CShutdownState_Run)> CShutdownState_Run_h({0x48, 0x89, 0x6C, 0x24, 0x18, 0x56, 0x48, 0x83, 0xEC, 0x30,
                                                              0x48, 0x8B, 0x0D, 0xCC, 0xCC, 0xCC, 0xCC},
                                                             &CShutdownState_Run, 1);
 
-bool CShutdownState_Run(uintptr_t aThis, uintptr_t aApp)
+bool CShutdownState_Run(RED4ext::CShutdownState* aThis, RED4ext::CGameApplication* aApp)
 {
     auto app = App::Get();
     auto pluginManager = app->GetPluginManager();
