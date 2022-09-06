c4e5a60f74444415fa5cccd89dde7288e83fbf8b
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2022-09-06 20:38:57 +0200

    Update addresses and dependencies for patch 1.60

--- src/dll/Image.cpp
+++ src/dll/Image.cpp
@@ -148,5 +148,5 @@ const RED4ext::SemVer& Image::GetProductVersion() const
 
 const std::vector<RED4ext::FileVer> Image::GetSupportedVersions() const
 {
-    return {RED4EXT_RUNTIME_1_52, RED4EXT_RUNTIME_1_52_HOTFIX_1};
+    return {RED4EXT_RUNTIME_1_60};
 }
