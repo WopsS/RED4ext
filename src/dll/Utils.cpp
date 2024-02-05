06bb89817fb567207bc163df96f4bf775643d878
Louis Poirier <contact@louispoirier.io>
2024-02-05 14:29:40 +0100

    feat: add timestamp to plugin log file name.
    Move current timestamp formatter from App to Utils as a reusable function.

--- src/dll/Utils.cpp
+++ src/dll/Utils.cpp
@@ -153,6 +153,18 @@ std::wstring Utils::FormatLastError()
     return FormatSystemMessage(err);
 }
 
+std::wstring Utils::FormatCurrentTimestamp()
+{
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+    // Convert to std::tm for formatting
+    std::tm now_tm = *std::localtime(&now_c);
+
+    return fmt::format(L"{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}", now_tm.tm_year + 1900,
+                        now_tm.tm_mon + 1, now_tm.tm_mday, now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec);
+}
+
 int32_t Utils::ShowMessageBoxEx(const std::wstring_view aCaption, const std::wstring_view aText, uint32_t aType)
 {
     return MessageBox(nullptr, aText.data(), aCaption.data(), aType);
